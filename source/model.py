38181563a351850319e4b03958e24c3146f62f24
RensterMaat <rens.termaat@gmail.com>
2023-04-07 16:01:06 +0200

    naive classifier

--- source/model.py
+++ source/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 
@@ -25,19 +26,45 @@ class Model(pl.LightningModule):
         pass
 
 
-class NaivePoolingClassifier(nn.Module):
-    def __init__(self, config):
-        pass
+class Classifier(nn.Module):
+    def __init__(self, config: dict) -> None:
+        super().__init__()
 
-    def forward(self, x):
-        pass
+        if config["final_activation"] == "softmax":
+            self.final_activation = nn.Softmax()
+        elif config["final_activation"] == "sigmoid":
+            self.final_activation = nn.Sigmoid()
+        else:
+            raise NotImplementedError
 
-    def get_heatmap(self, x):
-        pass
+
+class NaivePoolingClassifier(Classifier):
+    def __init__(self, config: dict) -> None:
+        super().__init__(config)
+
+        self.classifier = nn.Linear(config["n_features"], config["n_classes"])
+
+        if config["pooling_function"] == "max":
+            self.pooling = nn.AdaptiveMaxPool1d(1)
+        elif config["pooling_function"] == "mean":
+            self.pooling = nn.AdaptiveAvgPool1d(1)
+        else:
+            raise NotImplementedError
+
+    def forward(self, x: torch.Tensor, return_heatmap: bool = False) -> torch.Tensor:
+        per_patch_prediction = self.classifier(x)
+        logits = torch.t(self.pooling(torch.t(per_patch_prediction)))
+        slide_prediction = self.final_activation(logits)
+
+        if return_heatmap:
+            return slide_prediction, per_patch_prediction
+
+        return slide_prediction
 
 
 class AttentionClassifier(nn.Module):
     def __init__(self, config):
+        super().__init__()
         pass
 
     def forward(self, x):
@@ -49,6 +76,7 @@ class AttentionClassifier(nn.Module):
 
 class TransformerClassifier(nn.Module):
     def __init__(self, config):
+        super().__init__()
         pass
 
     def forward(self, x):
---
+++ test/test_model.py
@@ -0,0 +1,27 @@
+from source.model import (
+    NaivePoolingClassifier,
+    AttentionClassifier,
+    TransformerClassifier,
+    Model,
+)
+from source.data import PreextractedFeatureDataset, DataModule
+
+
+def test_naive_pooling_classifier():
+    config = {
+        "target": "label",
+        "n_features": 192,
+        "n_classes": 3,
+        "pooling_function": "max",
+        "final_activation": "softmax",
+    }
+    dataset = PreextractedFeatureDataset(
+        "/mnt/hpc/rens/hipt/data/fold_dir_test/fold_0/train.csv", config
+    )
+    x, _ = dataset[0]
+
+    model = NaivePoolingClassifier(config)
+
+    y_hat = model.forward(x)
+    # y_hat, heatmap = model.forward(x, return_heatmap=True)
+    print()
