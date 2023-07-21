773ade69ccf210bb5706d70824d4b98200909483
RensterMaat <rens.termaat@gmail.com>
2023-07-21 09:58:39 +0200

    random extractor

--- source/extract/extractors.py
+++ source/extract/extractors.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 from abc import ABC, abstractmethod
 
+
 class Extractor(nn.Module, ABC):
     def __init__(self) -> None:
         super().__init__()
@@ -14,4 +15,16 @@ class Extractor(nn.Module, ABC):
     @abstractmethod
     def transform(self, x: np.array) -> torch.Tensor:
         pass
+
+
+class RandomExtractor(Extractor):
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.config = config
+
+    def forward(self, x):
+        return np.random.normal(size=(x.shape[0], 32))
+    
+    def transform(self, x):
+        return x
 
