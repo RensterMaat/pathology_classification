9b627e4aa96e395875f6195b5e12ba60478a79f1
RensterMaat <rens.termaat@gmail.com>
2023-08-02 12:27:08 +0200

    fixed tessallate

--- source/extract/preprocess.py
+++ source/extract/preprocess.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import torch
 import json
+import math
 import numpy as np
 from torch.nn.functional import conv2d
 from skimage.filters import threshold_otsu
@@ -14,7 +15,11 @@ from typing import Optional
 from pathlib import Path
 from tqdm import tqdm
 
-from source.utils.utils import load_config, get_patch_coordinates_dir_name, list_all_slide_file_paths
+from source.utils.utils import (
+    load_config,
+    get_patch_coordinates_dir_name,
+    list_all_slide_file_paths,
+)
 
 
 class Preprocessor:
@@ -66,7 +71,9 @@ class Preprocessor:
             / slide.level_downsamples[self.config["extraction_level"]]
         )
         tiles = self.tessellate(segmentation, scaling_factor)
-        scaled_tiles = self.scale_tiles(tiles, scaling_factor)
+        scaled_tiles = self.scale_tiles(
+            tiles, slide.level_downsamples[self.config["preprocessing_level"]]
+        )
 
         self.save_tiles(scaled_tiles, slide_path)
 
@@ -112,6 +119,7 @@ class Preprocessor:
         Returns:
             scaled_tiles: dictionary containing the scaled tiles for each cross-section.
         """
+
         scaled_tiles = {}
         for cross_section in tiles:
             scaled_tiles[cross_section] = []
@@ -120,11 +128,8 @@ class Preprocessor:
                     (
                         pos,
                         (int(loc[0] * scaling_factor), int(loc[1] * scaling_factor)),
-                        (
-                            int(shape[0] * scaling_factor),
-                            int(shape[1] * scaling_factor),
-                        ),
-                    )
+                        shape,
+                    ),
                 )
 
         return scaled_tiles
@@ -162,12 +167,20 @@ class Preprocessor:
 
         Args:
             segmentation: segmentation of the whole-slide image as (height, width, cross-section).
+            scaling_factor: scaling factor between the extraction level and the highest magnification level.
 
         Returns:
             tile_information: dictionary containing the location and shape of the tiles for each cross-section.
         """
-        shape = [int(dim / scaling_factor) for dim in self.config["patch_dimensions"]]
-        # shape = self.config["patch_dimensions"]
+        segmentation_width, segmentation_height = (
+            segmentation.shape[1],
+            segmentation.shape[0],
+        )
+
+        patch_size = [
+            int(dim / scaling_factor) for dim in self.config["patch_dimensions"]
+        ]
+        patch_width, patch_height = patch_size[1], patch_size[0]
 
         if isinstance(segmentation, torch.Tensor):
             segmentation = segmentation.numpy()
@@ -177,116 +190,152 @@ class Preprocessor:
                 "Argument for `segmentation` has an invalid shape (expected 3 axes).",
             )
         # check if the shape is not larger than the segmentation
-        if shape[0] > segmentation.shape[0] or shape[1] > segmentation.shape[1]:
+        if patch_width > segmentation_width or patch_height > segmentation_height:
             raise ValueError(
                 "Argument for `shape` exceeds the height and/or width of the segmentation.",
             )
+
         # set the stride equal to the shape if it is None
-        stride = self.config["stride"] if "stride" in self.config.keys() else shape
+        stride = (
+            self.config["stride"]
+            if "stride" in self.config.keys() and self.config["stride"] is not None
+            else patch_size
+        )
+        horizontal_stride, vertical_stride = stride[1], stride[0]
+
+        # calulate the maximum number of patches that fit inside the image
+        max_horizontal_patches = (
+            segmentation_width - patch_width
+        ) // horizontal_stride + 1
+        max_vertical_patches = (
+            segmentation_height - patch_height
+        ) // vertical_stride + 1
 
         # initialize dictionary to store tile location and shapes
         tile_information = {}
 
-        # loop over the axes:
-        for i in range(segmentation.shape[-1]):
-            # get the cross-section
-            cross_section = segmentation[..., i]
-
-            # get bounding box for cross-section
+        for i, cross_section in enumerate(segmentation.transpose(2, 0, 1)):
             bounding_box = self.get_bounding_box(cross_section)
-            if bounding_box is not None:
-                # get the bounding box coordinates
-                top, bottom, left, right = bounding_box
-                # calculate the height and width
-                height = bottom - top
-                width = right - left
-                # calculate the added height and width to make it divisible
-                added_height = (ceil(height / shape[0]) * shape[0]) - height
-                added_width = (ceil(width / shape[1]) * shape[1]) - width
 
-                # determine the correction by splitting the height and width to be added
-                height_correction = (-floor(added_height / 2), ceil(added_height / 2))
-                width_correction = (-floor(added_width / 2), ceil(added_width / 2))
+            if bounding_box is None:
+                continue
 
-                # determine how far outside the corrected height and width would be
-                outside_top = max(0, -(top + height_correction[0]))
-                outside_bottom = max(
-                    0, (bottom + height_correction[1]) - segmentation.shape[0]
+            # create a boundix box and calculate the width and height
+            (
+                top,
+                bottom,
+                left,
+                right,
+            ) = bounding_box
+
+            bounding_box_width = right - left
+            bounding_box_height = bottom - top
+
+            # determine how many patches are needed in both dimensions to completely cover the bounding box
+            # two cases are possible:
+            # 1. the bounding box is smaller than the patch size -> default to one patch
+            # 2. the bounding box is larger than the patch size -> calculate the number of patches needed
+            horizontal_patches_needed_to_cover_bounding_box = (
+                max(
+                    math.ceil((bounding_box_width - patch_width) / horizontal_stride), 0
                 )
-                outside_left = max(0, -(left + width_correction[0]))
-                outside_right = max(
-                    0, (right + width_correction[1]) - segmentation.shape[1]
+                + 1
+            )
+            vertical_patches_needed_to_cover_bounding_box = (
+                max(
+                    math.ceil((bounding_box_height - patch_height) / vertical_stride), 0
                 )
+                + 1
+            )
 
-                # address the corrections for which there is overlap at the top and/or bottom
-                if (outside_top and not outside_bottom) or (
-                    outside_bottom and not outside_top
-                ):
-                    height_correction = (
-                        height_correction[0] + outside_top - outside_bottom,
-                        height_correction[1] + outside_top - outside_bottom,
-                    )
-                elif outside_top and outside_bottom:
-                    height_correction = (
-                        floor((shape[0] - added_height) / 2),
-                        -ceil((shape[0] - added_height) / 2),
-                    )
-                # address the corrections for which there is overlap on the left and/or right
-                if (outside_left and not outside_right) or (
-                    outside_right and not outside_left
-                ):
-                    width_correction = (
-                        width_correction[0] + outside_left - outside_right,
-                        width_correction[1] + outside_left - outside_right,
-                    )
-                elif outside_left and outside_right:
-                    width_correction = (
-                        floor((shape[1] - added_width) / 2),
-                        -ceil((shape[1] - added_width) / 2),
-                    )
-                # determine the top left coordinate as (x, y)
-                top_left = (left + width_correction[0], top + height_correction[0])
-                height = height - height_correction[0] + height_correction[1]
-                width = width - width_correction[0] + width_correction[1]
+            # the number of patches should never be more than the maximum number of patches
+            # that fit inside the image
+            horizontal_patches = min(
+                horizontal_patches_needed_to_cover_bounding_box, max_horizontal_patches
+            )
+            vertical_patches = min(
+                vertical_patches_needed_to_cover_bounding_box, max_vertical_patches
+            )
 
-                # crop the cross-section and prepare for convolution
-                crop = cross_section[
-                    top_left[1] : top_left[1] + height,
-                    top_left[0] : top_left[0] + width,
-                ][None, None, ...].astype(np.float32)
-                # define average filter
-                filter = (torch.ones(shape) / np.prod(shape))[None, None, ...]
-                # convolve crop with filter
-                filtered_crop = conv2d(
-                    torch.from_numpy(crop),
-                    weight=filter,
-                    bias=None,
-                    stride=stride,
-                    padding="valid",
-                )[0, 0, ...].numpy()
-                # find the region to extract tiles from
-                extraction_region = np.where(
-                    filtered_crop >= self.config["min_tissue_fraction"], 1, 0
+            # calculate the width and height spanned by the patches
+            width_spanned_by_horizontal_patches = (
+                horizontal_patches - 1
+            ) * horizontal_stride + patch_width
+            height_spanned_by_vertical_patches = (
+                vertical_patches - 1
+            ) * vertical_stride + patch_height
+
+            # Align the center of the patches with the center of the bounding box
+            horizontal_shift = (
+                bounding_box_width - width_spanned_by_horizontal_patches
+            ) // 2
+            left_margin = left + horizontal_shift
+
+            vertical_shift = (
+                bounding_box_height - height_spanned_by_vertical_patches
+            ) // 2
+            top_margin = top + vertical_shift
+
+            # Ensure that no patches fall outside the segmentation
+            if left_margin < 0:
+                left_margin = 0
+            if left_margin + width_spanned_by_horizontal_patches > segmentation_width:
+                horizontal_shift = (
+                    left_margin
+                    + width_spanned_by_horizontal_patches
+                    - segmentation_width
                 )
+                left_margin -= horizontal_shift
+            if top_margin < 0:
+                top_margin = 0
+            if top_margin + height_spanned_by_vertical_patches > segmentation_height:
+                vertical_shift = (
+                    top_margin
+                    + height_spanned_by_vertical_patches
+                    - segmentation_height
+                )
+                top_margin -= vertical_shift
 
-                # find the indices of the tiles that exceed the minimum faction of tissue
-                indices = np.nonzero(extraction_region)
-                # loop over indices to get all (top left) tile locations
-                positions = []
-                locations = []
-                for x, y in zip(indices[1], indices[0]):
-                    positions.append((int(x), int(y)))
-                    locations.append(
-                        (
-                            int(top_left[0] + x * stride[1]),
-                            int(top_left[1] + y * stride[0]),
-                        )
+            # crop the cross-section and prepare for convolution
+            crop = cross_section[
+                top_margin : top_margin + height_spanned_by_vertical_patches,
+                left_margin : left_margin + width_spanned_by_horizontal_patches,
+            ][None, None, ...].astype(np.float32)
+            # define average filter
+            filter = (torch.ones(patch_size) / np.prod(patch_size))[None, None, ...]
+            # convolve crop with filter
+            filtered_crop = conv2d(
+                torch.from_numpy(crop),
+                weight=filter,
+                bias=None,
+                stride=stride,
+                padding="valid",
+            )[0, 0, ...].numpy()
+            # find the region to extract tiles from
+
+            extraction_region = np.where(
+                filtered_crop >= self.config["min_tissue_fraction"], 1, 0
+            )
+
+            # find the indices of the tiles that exceed the minimum faction of tissue
+            indices = np.nonzero(extraction_region)
+            # loop over indices to get all (top left) tile locations
+            positions = []
+            locations = []
+            for x, y in zip(indices[1], indices[0]):
+                positions.append((int(x), int(y)))
+                locations.append(
+                    (
+                        int(left_margin + x * horizontal_stride),
+                        int(top_margin + y * vertical_stride),
                     )
-                # add information about the location and shape of the tiles
-                # to the dictionary
-                tile_information[str(i)] = [
-                    (pos, loc, tuple(shape)) for pos, loc in zip(positions, locations)
-                ]
+                )
+            # add information about the location and shape of the tiles
+            # to the dictionary
+            tile_information[str(i)] = [
+                (pos, loc, self.config["patch_dimensions"])
+                for pos, loc in zip(positions, locations)
+            ]
 
         return tile_information
 
@@ -321,10 +370,8 @@ class Preprocessor:
 def main(config):
     preprocessor = Preprocessor(config)
 
-    # for slide in tqdm(list(Path(config["slides_dir"]).glob("*.ndpi"))):
-    #     preprocessor(slide)
-    for slide in tqdm(list_all_slide_file_paths(config['slides_dir'])):
-        preprocessor(str(slide))
+    for slide in tqdm(list_all_slide_file_paths(config["slides_dir"])):
+        preprocessor(slide)
 
 
 if __name__ == "__main__":
