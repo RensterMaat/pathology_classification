029b74e2d4052a9ed1bef5d0dbaf8b5e6598778f
RensterMaat <rens.termaat@gmail.com>
2023-10-31 10:24:14 +0100

    commit hpc

--- source/classify/cross_validate.py
+++ source/classify/cross_validate.py
@@ -49,8 +49,8 @@ def main(config):
         # trainer.fit(model, datamodule)
         trainer.test(model, datamodule)
 
-        with open(Path(config["experiment_log_dir"]) / "config.yaml", "w") as f:
-            yaml.safe_dump(config, f)
+    with open(Path(config['experiment_log_dir']) / 'config.yaml', 'w') as f:
+        yaml.safe_dump(config, f)
 
 
 if __name__ == "__main__":
