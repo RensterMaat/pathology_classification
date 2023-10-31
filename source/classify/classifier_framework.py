fix bug in auc calculation

source/classify/classifier_framework.py
@@ -101,14 +101,13 @@ class ClassifierFramework(pl.LightningModule):
         loss = self.criterion(y_hat, y)
         self.train_auc.update(y_hat[0, 1], y[0, 1].int())
 
-        self.log_dict(
-            {
-                f"fold_{self.config['fold']}/train_loss": loss,
-                f"fold_{self.config['fold']}/train_auc": self.train_auc.compute(),
-            }
-        )
+        self.log_dict(f"fold_{self.config['fold']}/train_loss", loss)
 
         return loss
+
+    def on_train_epoch_end(self) -> None:
+        self.log(f"fold_{self.config['fold']}/train_auc", self.train_auc.compute())
+        self.train_auc.reset()
 
     def validation_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
         """
@@ -127,14 +126,13 @@ class ClassifierFramework(pl.LightningModule):
         loss = self.criterion(y_hat, y)
         self.val_auc.update(y_hat[0, 1], y[0, 1].int())
 
-        self.log_dict(
-            {
-                f"fold_{self.config['fold']}/val_loss": loss,
-                f"fold_{self.config['fold']}/val_auc": self.val_auc.compute(),
-            }
-        )
+        self.log_dict(f"fold_{self.config['fold']}/val_loss", loss)
 
         return loss
+
+    def on_validation_epoch_end(self) -> None:
+        self.log(f"fold_{self.config['fold']}/val_auc", self.val_auc.compute())
+        self.val_auc.reset()
 
     def test_step(self, batch: torch.Tensor, batch_idx: int) -> None:
         """
@@ -170,11 +168,6 @@ class ClassifierFramework(pl.LightningModule):
         self.test_outputs.append(
             [slide_id, int(y[0, 1].detach().cpu()), float(y_hat[0, 1].detach().cpu())]
         )
-        self.log_dict(
-            {f"test/fold_{self.config['fold']}_auc": self.test_auc.compute()},
-            on_step=False,
-            on_epoch=True,
-        )
 
     def on_test_epoch_end(self):
         """
@@ -187,6 +180,10 @@ class ClassifierFramework(pl.LightningModule):
         results_dir.mkdir(exist_ok=True, parents=True)
         results.to_csv(
             results_dir / f"fold_{self.config['fold']}_test_output.csv", index=False
+        )
+
+        self.log_dict(
+            {f"test/fold_{self.config['fold']}_auc": self.test_auc.compute()},
         )
 
     def configure_optimizers(self) -> torch.optim.Optimizer: